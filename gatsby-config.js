6db0c41d8124090991d6d03353540ff69c3ea203
Denham Rogers <denhamrogers10@gmail.com>
2019-12-09 20:08:44 -0600

    updated GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,7 @@ module.exports = {
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        defaultDataLayer: { platform: "Denham Rogers" },
       },
     },
     {
